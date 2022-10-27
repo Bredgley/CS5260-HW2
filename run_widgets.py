740c1193cdc3fba483cd1f1ae56585abda84a778
Brennan <bredgley@gmail.com>
2022-10-26 19:21:30 -0600

    Added json object parsing to create the new object we are going to upload to the next bucket

--- run_widgets.py
+++ run_widgets.py
@@ -4,12 +4,22 @@ from boto3 import client
 from botocore.exceptions import ClientError
 
 def check_items():
-
-    conn = client('s3')  # again assumes boto.cfg setup, assume AWS S3
-    for key in conn.list_objects(Bucket='usu-cs5260-ignite-dist')['Contents']:
+    wait_end = ""
+    bucket = 'usu-cs5260-ignite-dist'
+    key = 'hello world.txt'
+    
+    # while wait_end != "end"
+    conn = client('s3')  
+    for key in conn.list_objects(Bucket=bucket)['Contents']:
         print(key['Key'])
+        s3 = boto3.client('s3')
+        obj = s3.get_object(Bucket=bucket, Key=key['Key'])
+        j_data = json.loads(obj['Body'].read())
+        print(j_data)
+        print(j_data['type'])
+        
 
-def put_s3():
+def create_widget_s3(j_data):
     
     DDB = boto3.client('s3', region_name='us-east-1')
     
@@ -17,14 +27,17 @@ def put_s3():
         response = DDB.put_item(
             TableName='<FMI_1>',
             Item={
-                'product_name': {
+                'widget_id': {
                     'S': '<FMI_2>'
                 },
-                'product_id': {
+                'owner': {
                     'S': '<FMI_3>'
                 },
-                'price_in_cents':{
-                    'N': '<FMI_4>' #number passed in as a string (ie in quotes)
+                'label':{
+                    'S': '<FMI_4>' #number passed in as a string (ie in quotes)
+                },
+                'description':{
+                    'S': "<FMI_5>"
                 },
                 'description':{
                     'S': "<FMI_5>"
@@ -81,11 +94,11 @@ def put_dynamodb():
         if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
             pass
         
-session = boto3.Session()
-s3_client = session.client('s3')
-b = s3_client.list_buckets()
-for item in b['Buckets']:
-    print(item['Name'])
+# session = boto3.Session()
+# s3_client = session.client('s3')
+# b = s3_client.list_buckets()
+# for item in b['Buckets']:
+#     print(item['Name'])
     
 # try:
 if sys.argv[1] != 's3' and sys.argv[1] != 'dynamodb':
